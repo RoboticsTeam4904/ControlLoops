28a6f5960471d8c929a1f7885d0ea120727a50f7
Evan Steirman <evastei@nus11172-10-evastei.nueva.local>
2018-12-14 17:51:58 -0800

    edited robot.py

--- Simulation/robot.py
+++ Simulation/robot.py
@@ -1,10 +1,24 @@
 import time
-j = 0
+import turtle
+
+acc = 0.05
+
+j = 1
 pos = 0
 vel = 0
-acc = 2
-while j < 10:
+angle = 0.1
+turtle.speed(0)
+while j < 60:
 	vel = j*acc
 	pos = pos + vel
 	print(pos)
+	turtle.pendown()
+	turtle.forward(vel)
 	j = j + 1
+time.sleep(1)
+while j > 0:
+	vel = j*acc
+	pos = pos + vel
+	print(pos)
+	turtle.forward(vel)
+	j = j - 1
